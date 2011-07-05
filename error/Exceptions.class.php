29349e16658c7e9dcceb5c1c2a0a5bbfd7b8de77
Pieter Colpaert <pieter@iRail.be>
2011-07-05 09:55:42 +0200

    added exception

--- error/Exceptions.class.php
+++ error/Exceptions.class.php
@@ -7,7 +7,6 @@
  * License: AGPLv3
  */
 
-
 /**
  * The abstract function of TDT Exception
  */
@@ -15,17 +14,59 @@ class AbstractTDTException extends Exception{
      abstract public function getDoc();
      abstract public function getErrorCode();
 
-     // Redefine the exception so message isn't optional
-     public function __construct($message, $code = 0) {
-	  // some code
+     public function __construct($message) {
+	  //Needs to be overridden - getErrorCode will return a HTTP-like errorcode according to REST specs
 	  $code = $this->getErrorCode();
-	  // make sure everything is assigned properly
 	  parent::__construct($message, $code);
-     }
-
-     public function customFunction() {
-	  echo "A custom function for this type of exception\n";
      }
 }
 
+/**
+ * These are HTTP 400 errors: Parameter or Methods not found
+ *
+ */
+class MethodOrModuleNotFoundTDTException extends AbstractTDTException{
+     public function getDoc(){
+	  return "When a method or module is not found this Exception is thrown. The constructor expects the name of the module or the name of the method. This is a 404 error: not found";
+     }
+
+     public function __construct($m) {
+	  parent::__construct("Method or module not found: " . $m);
+     }
+
+     public function getErrorCode(){
+	  return 404;
+     }
+}
+
+class ParameterTDTException extends AbstractTDTException{
+     public function getDoc(){
+	  return "This exception is thrown when a parameter is incorrect. The constructor needs a parameter on which it blocked and a message why it blocked: Parameter not found or Parameter incorrect";
+     }
+
+     public function getErrorCode(){
+	  return 401;
+     }
+
+     public function __construct($parameter, $message){
+	  parrent::__construct("Parameter not found or incorrect: " . $parameter);
+     }
+}
+
+/**
+ * These are HTTP 500 errors: internal server errors
+ */ 
+class CouldNotGetDataTDTException extends AbstractTDTException{
+     public function getDoc(){
+	  return "This exception is thrown when the data could not be resolved.";
+     }
+
+     public function getErrorCode(){
+	  return 501;
+     }
+
+     public function __construct($datasourcename){
+	  parrent::__construct("This could not be connected: " . $datasourcename);
+     }
+}
 ?>
