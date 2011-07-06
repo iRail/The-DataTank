cf986bd987f18f62335c1dcd3210b1700ae6c1e7
Pieter Colpaert <pieter@iRail.be>
2011-07-06 12:06:31 +0200

    Added iRail classes and fixed parameter issues

--- modules/iRail/Liveboard.class.php
+++ modules/iRail/Liveboard.class.php
@@ -1,27 +1,39 @@
 <?php
+ini_set("include_path",".:modules/iRail/");
 include_once("modules/AMethod.php");
-//test php method
+include_once("Stations.class.php");
+include_once("iRailTools.class.php");
+
 class Liveboard extends AMethod{
 
-     private $parameters;
-     private $documentation;
+     private $lang,$system;
+     
 
      public function __construct(){
+	  parent::__construct("Liveboard");
      }
 
      public static function getParameters(){
-	  return array("stationId" => "Station id is an id of a station as specified by the iRail API: example: BE.NMBS.0942484");
+	  return array("station" => "This is a name of a station or an ID as specified by the iRail API: example: BE.NMBS.0942484",
+		       "time" => "time of the requested liveboard concatenated. The 3pm would be: 1500",
+		       "direction" => "Do you want to have the arrivals or the departures. Values: arrival or departures",
+		       "lang" => "Language for the stations",
+		       "system" => "The name of the public transport company: for instance De Lijn, or NMBS"
+	       );
      }
 
      public static function getRequiredParameters(){
-	  return array("stationId");
+	  return array("station");
      }
 
-     public function setParameters($params){
-	  //foreach element in the array, check if the parametershash contains
-	  //such a key, if so set the value to the correct key.	  
-	  foreach($params as $key=>$value){  	      
-	       $this->parameters[$key] = $value;
+     public function setParameter($key,$val){
+	  if($key == "lang"){
+	       $this->lang = $val;
+	  }
+	  if($key == "system"){
+	       if(in_array($val, iRailTools::ALLOWED_SYSTEMS)){
+		    $this->system = $val;
+	       }
 	  }
      }
 
@@ -31,12 +43,11 @@ class Liveboard extends AMethod{
      }
      
      public function allowedPrintMethods(){
-	  $printmethods;
 	  $printmethods = array("xml");
      }
 
      public static function getDoc(){
-	  return "This is a dummy class, inherits from AMethod";
+	  return "Liveboard will return the next arrivals or departures in a station";
      }
 }
 
