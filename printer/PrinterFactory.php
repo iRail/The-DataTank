75021b000209b0afb65a5532a209a52dd3502026
Pieter Colpaert <pieter@iRail.be>
2011-07-08 21:31:22 +0200

    Didn't compile due to someone forgot a semicolon. Lame.

--- printer/PrinterFactory.php
+++ printer/PrinterFactory.php
@@ -3,14 +3,14 @@
 include_once("printer/Printer.php");
 
 class PrinterFactory{
-    public static function getPrinter($rootname, $format, $rootname,$objectToPrint){
-        $callback = null
-	    if(isset($_GET["callback"]) && $format=="json"){
-            $format = "jsonp";
-            $callback = $_GET["callback"];
-        }
-        
-        return new Printer($rootname, $objectToPrint, $format, $callback);
+     public static function getPrinter($rootname, $format, $rootname,$objectToPrint){
+	 $callback = null;
+	 if(isset($_GET["callback"]) && $format == "json"){
+	      $format = "jsonp";
+	      $callback = $_GET["callback"];
+	 }
+	 
+	 return new Printer($rootname, $objectToPrint, $format, $callback);
     }
 }
 ?>
