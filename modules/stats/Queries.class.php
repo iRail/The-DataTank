a69884b0c0063b326324c71fd1a64de4f6c590fe
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-07-11 15:03:33 +0200

    Stats is a module now, feeding our own stats analysis tool on the datatank itself....catch 22 ftw. You can click some criteria now that allow you specify your analysis.

--- modules/stats/Queries.class.php
+++ modules/stats/Queries.class.php
@@ -1,37 +1,97 @@
 <?php
-/* Copyright (C) 2011 by iRail vzw/asbl
- *
- * Author: Jan Vansteenlandt <jan aŧ iRail.be>
- * Author: Pieter Colpaert <pieter aŧ iRail.be>
- * License: AGPLv3
- *
- * Lists the number of queries to the API per day
- */
+  /* Copyright (C) 2011 by iRail vzw/asbl
+   *
+   * Author: Jan Vansteenlandt <jan aŧ iRail.be>
+   * Author: Pieter Colpaert <pieter aŧ iRail.be>
+   * License: AGPLv3
+   *
+   * Lists the number of queries to the API per day
+   */
 
 include_once("modules/AMethod.php");
 
 class Queries extends AMethod{
+
+     private $module; // must be set! Contains the value of the module that needs to be analysed.
+     private $method; // if set only look at certain data from a certain method within the given module.
+     private $errors = ""; // if set, get data from errors table. ( not set or true )
+     private $queryResults;
+     
 
      public function __construct(){
 	  parent::__construct("Queries");
      }
 
      public static function getParameters(){
-	  return array();
+	  return array("mod" => "Name of a module that needs to be analysed, must be set !",
+		       "meth" => "Name of a method within the given module, is not required.",
+		       "err" => "If set then the analysis will get it's data from the error table if not from the request table."
+	       );
      }
 
      public static function getRequiredParameters(){
-	  return array();
+	  return array("mod");
      }
 
      public function setParameter($key,$val){
+	  if($key == "mod"){
+	       $this->module = $val;
+	  }elseif($key == "meth"){
+	       $this->method = $val;
+	  }elseif($key == "err"){
+	       $this->errors = $val;
+	  }
+     }
 
+     public function getData(){
+
+	  /* Connect to mysql database */
+	  $link = mysqli_connect(
+	       'localhost',                    /* The host to connect to */
+	       Config::$MySQL_USER_NAME,       /* The user to connect with the MySQL database */
+	       Config::$MySQL_PASSWORD,        /* The password to use to connect with the db  */
+	       'logging');                     /* The default database to query */
+
+	  if (!$link) {
+	       printf("Can't connect to MySQL Server. Errorcode: %s\n", mysqli_connect_error());
+	       exit;
+	  }
+
+          /* Send a query to the server */
+	 
+	  if($this->errors == ""){
+	       $databasetable = "requests";
+	  }else{
+	       $databasetable = "errors";
+	  }	  
+
+	  $queryString = 'SELECT count(1) as amount, time FROM '
+	       . $databasetable . ' WHERE url_request REGEXP \''. $this->module . '/'.$this->method .
+	       '\' group by from_unixtime(time,\'%D %M %Y\')';
+	  //echo "queryString is ". $queryString;
+	  
+	  if ($result = mysqli_query($link,$queryString)) {   
+
+	       /* Fetch the results of the query */
+	       $this->queryResults = new QueryResults();
+	       
+	       while( $row = mysqli_fetch_assoc($result) ){
+		    $amount = $row['amount'];
+		    $time  = $row['time'];
+		    $this->queryResults->result[$time] = $amount;
+	       }
+
+	       /* Destroy the result set and free the memory used for it. */
+	       mysqli_free_result($result);
+	  }
+
+          /* Close the connection */
+	  mysqli_close($link);
      }
 
      public function call(){
-	  $q = new QueryResults();
-	  $q->visitsPerDay = array ('1310039116000'=>5,'1310125515000'=>28);
-	  return  $q;
+	  $this->getData();
+	  return $this->queryResults;
      }
      
      public function allowedPrintMethods(){
@@ -39,11 +99,11 @@ class Queries extends AMethod{
      }
 
      public static function getDoc(){
-	  return "Lists the number of queries to this datatank instance per day";
+	  return "Lists the number of queries(requests/errors) to this datatank instance per day";
      }
 }
 
 class QueryResults{
-     public $visitsPerDay;
+     public $result;
 }
 ?>
--- stats/index.php
+++ stats/index.php
@@ -1,63 +1,57 @@
 <?php
 
-/* Copyright (C) 2011 by iRail vzw/asbl
- *
- * Author: Jan Vansteenlandt <jan aŧ iRail.be>
- * License: AGPLv3
- *
- * This file displays some basic analysis of the request logging and error logging.
- */
+  /* Copyright (C) 2011 by iRail vzw/asbl
+   *
+   * Author: Jan Vansteenlandt <jan aŧ iRail.be>
+   * License: AGPLv3
+   *
+   * This file displays some basic analysis of the request logging and error logging.
+   */
 ini_set("include_path", "../");
 ini_set("error_reporting", E_ALL);
 
 
 include_once("Config.class.php");
 
-/* Connect to mysql database */
-$link = mysqli_connect(
-     'localhost',  /* The host to connect to */
-     Config::$MySQL_USER_NAME,       /* The user to connect with the MySQL database */
-     Config::$MySQL_PASSWORD,        /* The password to use to connect with the db  */
-     'logging');     /* The default database to query */
-
-if (!$link) {
-     printf("Can't connect to MySQL Server. Errorcode: %s\n", mysqli_connect_error());
-     exit;
-}
 
 $data = array();
 $time = array();
 
-/* Send a query to the server */
-if ($result = mysqli_query($link, 
-			   'SELECT count(1) as number, time as day FROM requests group by from_unixtime(time,\'%Y %D %M\')')) {   
 
-     /* Fetch the results of the query */
-     while( $row = mysqli_fetch_assoc($result) ){
-	  $data[] = $row['number'];
-	  $day[]  = $row['day'];
-     }
-
-     /* Destroy the result set and free the memory used for it */
-     mysqli_free_result($result);
-}
-
-/* Close the connection */
-mysqli_close($link);
 
 //*********************************** Start output
 
 include_once("templates/TheDataTank/header.php");
 
 ?>
-     <!--[if lte IE 8]><script language="javascript" src="flot/excanvas.min.js"></script><![endif]-->
+<!--[if lte IE 8]><script language="javascript" src="flot/excanvas.min.js"></script><![endif]-->
      <script language="javascript" src="/templates/TheDataTank/js/flot/jquery.js"></script>
      <script language="javascript" src="/templates/TheDataTank/js/flot/jquery.flot.js"></script>
 
-    <h1>Request logs</h1>
+     <h1 id="title"></h1>
+     <br>
      <div id="placeholder" style="width:600px;height:300px;"></div>
+
      <p>
-     <input id="submit" type="button" value="Load new data">
+     Datasource
+     <select id="datasource">
+     <option>requests</option>
+     <option>errors</option>
+     </select>
+     </p>
+     <p>
+     Module
+     <select id="module">
+     <option>iRail</option>
+     </select>
+     Method
+     <select id="method">
+     <option>Liveboard</option>
+     <option></option>
+     </select>
+     </p>
+     <p>
+     <input id="submit" type="button" value="Fetch results">
      </p>
      <script language="javascript">
      var $ = jQuery.noConflict();
@@ -66,21 +60,21 @@ $(function () {
 	  // get an array to display, in this case a single point is a pair : [ unixtime, amount of requests ] 
 	  var dataArray = [ <?php
 			    if(sizeof($data)>0){
-				 $javascripttime = $day[0] * 1000;
+				 $javascripttime = $time[0] * 1000;
 				 echo "[".$javascripttime . "," . $data[0] . "]";
 				 for($i=1; $i<sizeof($data); $i++){
-				      $javascripttime = $day[$i] * 1000;
+				      $javascripttime = $time[$i] * 1000;
 				      echo ", ". "[".$javascripttime . "," . $data[$i] . "]";
 				 }
 			    }			  
 			    ?>];
 	  // get the array to display ticks on the x-axis (time ticks)
 	  var xArray = [<?php
-			if(sizeof($day)>0){
-			     $javascripttime = $day[0] * 1000;
+			if(sizeof($time)>0){
+			     $javascripttime = $time[0] * 1000;
 			     echo "[".$javascripttime ."]";
-			     for($i=1; $i<sizeof($day); $i++){
-				  $javascripttime = $day[$i] * 1000;
+			     for($i=1; $i<sizeof($time); $i++){
+				  $javascripttime = $time[$i] * 1000;
 				  echo ", ". "[".$javascripttime .  "]";
 			     }
 			}	
@@ -88,7 +82,7 @@ $(function () {
 
 	  var data = [
 	       {
-	       label: "Request logging",
+		    //label: "Request logging",
 	       data: dataArray
 	       }
 	       ];
@@ -123,97 +117,108 @@ $(function () {
 	  }
 	  };
 
-	  var plotarea = $("#placeholder");
-	  $.plot( plotarea , data, options);
+	  $("#placeholder").text("Select your criteria and click on \"Fetch results\".");	  
      });
 
 
 $(document).ready(function(){
 
-	$('#submit').click(function(){
-		$.ajax({
-			type : 'POST',
-			url : 'http://localhost/stats/Queries/?format=json',
-			dataType : 'json',
-			success : function(result){
-			       plotChart(result);
-			},
-			error : function(XMLHttpRequest, textStatus, errorThrown) {
-			       alert('Something went wrong. ' + errorThrown);
-			}
-		});
-		return false;
-	});
-});
-
+	  $('#submit').click(function(){ 
+		    var moduleName = $('#module').val();
+		    var methodName = $('#method').val();
+		    var args = "&mod="+ moduleName;
+		    if(methodName != ""){
+			 args+="&meth="+methodName;
+		    }
+		    var table = $('#datasource').val();
+		    if(table != "requests"){
+			 args+="&err=true";
+		    }
+		    
+		    var url = 'http://localhost/stats/Queries/?format=json'+args;
+		    $.ajax({
+			 type : 'POST',
+				   url : 'http://localhost/stats/Queries/?format=json'+args,
+				   dataType : 'json',
+				   success : function(result){
+				   plotChart(result);
+			      },
+				   error : function(XMLHttpRequest, textStatus, errorThrown) {
+				   alert('Something went wrong. ' + errorThrown);
+			      }
+			 });
+		    return false;
+	       });
+     });
 
 /* plotChart with own Data !! */     
 
 function plotChart(dataArray) { 
 	 
-     var dataset = dataArray["visitsPerDay"];
-
+     /* dataset given, get the resulting array of the result object*/
+     var dataset = dataArray["result"];
+     
+     /* our dataArray contains data that needs to be kinda tweaked -> unix to javascripttime */
      var dataToDisplay = [];
-
-     for (var i in dataset) {
-	  dataToDisplay.push([i,dataset[i]]);
+     
+     for (var i in dataset) {	  
+	  dataToDisplay.push([i*1000,dataset[i]]);
      }
 
-     // get the array to display ticks on the x-axis (time ticks)
-     var xArray = [];
+     if(dataToDisplay.length > 0){
+	  /* construct the x-axis array, again conversion from unix to javascripttime */
+	  var xArray = [];
 
-     for(var i in dataset){
-	  xArray.push(i);
-     }
-	 
-
-     var data = [
-	  {
-	  label: "Request logging",
-	  data: dataToDisplay
+	  for(var i in dataset){
+	       xArray.push(i*1000);
 	  }
-	  ];
+
+	  var data = [
+	       {
+		    //label: "Request logging",
+	       data: dataToDisplay
+	       }
+	       ];
 	  
-     var options = {
-     legend: {
-	  show: true,
-	  margin: 10,
-	  backgroundOpacity: 0.5
-     },
-     points: {
-	  show: true,
-	  radius: 3,
-	  clickable: true,
-	  hoverable: true
+	  var options = {
+	  legend: {
+	       show: true,
+	       margin: 10,
+	       backgroundOpacity: 0.5
+	  },
+	  points: {
+	       show: true,
+	       radius: 3,
+	       clickable: true,
+	       hoverable: true
 
-     },
-     bars: {
-	  show: true
-     },
-     grid: {
-	  borderWidth:0,
-	  backgroundColor: "white"
-     },
-     xaxis: {
-	  ticks: xArray,
-	  mode: "time",
-	  timeformat: "%d/%m/%y"
-     },
-     yaxis: {
-	       
+	  },
+	  bars: {
+	       show: true
+	  },
+	  grid: {
+	       borderWidth:0,
+	       backgroundColor: "white"
+	  },
+	  xaxis: {
+	       ticks: xArray,
+	       mode: "time",
+	       timeformat: "%d/%m/%y"
+	  },
+	  yaxis: {
+	  }
+	  };
+
+	  var plotarea = $("#placeholder");
+	  $.plot( plotarea , data, options );
+     }else{
+	  $("#placeholder").text("No logging data available for the selected criteria.");
      }
-     };
-
-     var plotarea = $("#placeholder");
-     $.plot( plotarea , data, options );
 };
 
 </script>
-
 <?php
-
 include_once("templates/TheDataTank/footer.php");
-
 ?>
 
 
