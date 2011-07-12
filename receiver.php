176532eb9702cfb6b3903a5334725cb6f150c2c4
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-07-12 09:23:26 +0200

    Merge conflict

--- receiver.php
+++ receiver.php
@@ -1,5 +1,4 @@
-<?php
-/* Copyright (C) 2011 by iRail vzw/asbl
+<?php/* Copyright (C) 2011 by iRail vzw/asbl
  * Author:  Jan Vansteenlandt <jan aŧ iRail.be>
  * Author:  Pieter Colpaert <pieter aŧ iRail.be>
  * License: AGPLv3
@@ -8,83 +7,85 @@
  *
  * Notice: If this file reaches more than 100 lines a rewrite is needed
  */
-include_once("printer/PrinterFactory.php");
-include_once("error/Exceptions.class.php");
-include_once("requests/RequestLogger.class.php");
-include_once("error/ErrorHandler.class.php");
-include_once("modules/FederatedModules.php");
-include_once("modules/General/Federated.class.php");
-include_once("Config.class.php");
+include_once ("printer/PrinterFactory.php");
+include_once ("error/Exceptions.class.php");
+include_once ("requests/RequestLogger.class.php");
+include_once ("error/ErrorHandler.class.php");
+include_once ("modules/FederatedModules.php");
+include_once ("modules/General/Federated.class.php");
+include_once ("Config.class.php");
 
-$methodname;$format;$result;
-try{
-/*
- Get the format, keys and values, 
- pass them to our PrinterFactory which returns our formatter, if the formatter exists ofcourse.
-*/
+$methodname;
+$format;
+$result;
+try {
+	/*
+	 Get the format, keys and values,
+	 pass them to our PrinterFactory which returns our formatter, if the formatter exists ofcourse.
+	 */
 
-     $format = "";
+	$format = "";
 
-     if(isset($_GET["format"])){
-	  $format = $_GET["format"];
-     }
-     if($format == ""){
-	  $format = "Xml";
-     }
+	if(isset($_GET["format"])) {
+		$format = $_GET["format"];
+	}
+	if($format == "") {
+		$format = "Xml";
+	}
 
-//make sure the first letter is uppercase and the rest is lowercase
-     $format = ucfirst(strtolower($format));
+	//make sure the first letter is uppercase and the rest is lowercase
+	$format = ucfirst(strtolower($format));
 
-/*
- Check if the method exists in some module and fill in the required parameters.
- Return an exception if a module or method does not exist. If some required parameters
- aren't passed, we throw a special exception for that as well.
-*/
-     $result;
-     if(isset($_GET["module"])){
-	  $module = $_GET["module"];
+	/*
+	 Check if the method exists in some module and fill in the required parameters.
+	 Return an exception if a module or method does not exist. If some required parameters
+	 aren't passed, we throw a special exception for that as well.
+	 */
+	$result;
+	if(isset($_GET["module"])) {
+		$module = $_GET["module"];
 
-	  if(isset($_GET["method"])){
-	       $methodname = $_GET["method"];
+		if(isset($_GET["method"])) {
+			$methodname = $_GET["method"];
 
-	       if(file_exists("modules/$module/$methodname.class.php")){
-		    //get the new method
-		    include_once("modules/$module/$methodname.class.php");
-		    
-		    $method = new $methodname();
+			if(file_exists("modules/$module/$methodname.class.php")) {
+				//get the new method
+				include_once ("modules/$module/$methodname.class.php");
 
-		    // check if the given format is allowed by the method
-		    // if not, throw an exception and return the allowed formats
-		    // to the user.
-		    if(!in_array(strtolower($format),$method->allowedPrintMethods())){
-			 throw new FormatNotAllowedTDTException($format,$method);
-		    }
-	       }else if(array_key_exists($module,FederatedModules::$modules)){
-		    $method = new Federated($module,$methodname,FederatedModules::$modules[$module]);
-	       }else{
-		    throw new MethodOrModuleNotFoundTDTException($module . "/" .$method);
-	       }
-	       //execute the method when no error occured
-	       $result = $method->call();
-	  }
-     }else{
-	  throw new MethodOrModuleNotFoundTDTException("No module");
-     }
-     /*
-       Now print the bloody thing in the preferred format or if none given and if allowed our default format.
-      */
-     $rootname = $methodname;
-     $rootname = strtolower($rootname);
-     $printer = PrinterFactory::getPrinter($rootname, $format,$rootname,$result);
-     $printer->printAll();
-}catch(Exception $e){
-     //Oh noes! An error occured! Let's send this to our error handler for logging purposes
-     ErrorHandler::logException($e);
+				$method = new $methodname();
+
+				// check if the given format is allowed by the method
+				// if not, throw an exception and return the allowed formats
+				// to the user.
+				if(!in_array(strtolower($format), $method -> allowedPrintMethods())) {
+					throw new FormatNotAllowedTDTException($format, $method);
+				}
+			}else if(array_key_exists($module, FederatedModules::$modules)) {
+				$method = new Federated($module, $methodname, FederatedModules::$modules[$module]);
+			} else {
+				throw new MethodOrModuleNotFoundTDTException($module . "/" . $method);
+			}
+			//execute the method when no error occured
+			$result = $method -> call();
+		}
+	} else {
+		throw new MethodOrModuleNotFoundTDTException("No module");
+	}
+	/*
+	 Now print the bloody thing in the preferred format or if none given and if allowed our default format.
+	 */
+	$rootname = $methodname;
+	$rootname = strtolower($rootname);
+	$printer = PrinterFactory::getPrinter($rootname, $format, $rootname, $result);
+	$printer -> printAll();
+} catch(Exception $e) {
+	//Oh noes! An error occured! Let's send this to our error handler for logging purposes
+	ErrorHandler::logException($e);
 }
 
 /*
  We log this request in any case! Even if it's not a valid one.
-*/
+ */
 RequestLogger::logRequest();
 ?>
 
--- stats/index.php
+++ stats/index.php
@@ -1,201 +1,185 @@
-<?php
-
-  /* Copyright (C) 2011 by iRail vzw/asbl
-   *
-   * Author: Jan Vansteenlandt <jan aŧ iRail.be>
-   * License: AGPLv3
-   *
-   * This file displays some basic analysis of the request logging and error logging.
-   */
+<?php/* Copyright (C) 2011 by iRail vzw/asbl
+ *
+ * Author: Jan Vansteenlandt <jan aŧ iRail.be>
+ * License: AGPLv3
+ *
+ * This file displays some basic analysis of the request logging and error logging.
+ */
 ini_set("include_path", "../");
 ini_set("error_reporting", E_ALL);
 
-
-include_once("Config.class.php");
-
+include_once ("Config.class.php");
 
 $data = array();
 $time = array();
 
-
-
 //*********************************** Start output
 
-include_once("templates/TheDataTank/header.php");
-
-?>
+include_once ("templates/TheDataTank/header.php");?>
 <!--[if lte IE 8]><script language="javascript" src="flot/excanvas.min.js"></script><![endif]-->
-     <script language="javascript" src="/templates/TheDataTank/js/flot/jquery.js"></script>
-     <script language="javascript" src="/templates/TheDataTank/js/flot/jquery.flot.js"></script>
+<script language="javascript" src="/templates/TheDataTank/js/flot/jquery.js"></script>
+<script language="javascript" src="/templates/TheDataTank/js/flot/jquery.flot.js"></script>
 
-     <h1 id="title"></h1>
-     <br>
-     <div id="placeholder" style="width:600px;height:300px;"></div>
-     
-     <p>
-     Datasource
-     <select id="datasource">
-     <option>requests</option>
-     <option>errors</option>
-     </select>
-     </p>
-     <p>
-     Module
-     <select id="module">
-       <option>iRail</option>
-     </select>
-     Method
-     <select id="method">
-       <option>Liveboard</option>
-       <option></option>
-     </select>
-     </p>
-     <p>
-     <input id="submit" type="button" value="Fetch results">
-     </p>
-     <script language="javascript">
-     var $ = jQuery.noConflict();
-$(function () {
-	  $("#placeholder").text("Select your criteria and click on \"Fetch results\".");
-});
+<h1 id="title"></h1>
+<br>
+<div id="placeholder" style="width:600px;height:300px;">
+</div>
 
+<p>
+	Datasource
+	<select id="datasource">
+		<option>requests</option>
+		<option>errors</option>
+	</select>
+</p>
+<p>
+	Module
+	<select id="module">
+		<option>iRail</option>
+	</select>
+	Method
+	<select id="method">
+		<option>Liveboard</option>
+		<option></option>
+	</select>
+</p>
+<p>
+	<input id="submit" type="button" value="Fetch results">
+</p>
+<script language="javascript">var $ = jQuery.noConflict();
+	$( function () {
+		$("#placeholder").text("Select your criteria and click on \"Fetch results\".");
+	});
+	$(document).ready( function() {
 
-$(document).ready(function(){
+		$('#submit').click( function() {
+			var moduleName = $('#module').val();
+			var methodName = $('#method').val();
+			var args = "&mod="+ moduleName;
+			if(methodName != "") {
+				args+="&meth="+methodName;
+			}
+			var table = $('#datasource').val();
+			if(table != "requests") {
+				args+="&err=true";
+			}
 
-	  $('#submit').click(function(){ 
-		    var moduleName = $('#module').val();
-		    var methodName = $('#method').val();
-		    var args = "&mod="+ moduleName;
-		    if(methodName != ""){
-			 args+="&meth="+methodName;
-		    }
-		    var table = $('#datasource').val();
-		    if(table != "requests"){
-			 args+="&err=true";
-		    }
-		    
-		    var url = 'http://localhost/stats/Queries/?format=json'+args;
-		    $.ajax({
-			 type : 'POST',
-				   url : 'http://localhost/stats/Queries/?format=json'+args,
-				   dataType : 'json',
-				   success : function(result){
-				   plotChart(result);
-			      },
-				   error : function(XMLHttpRequest, textStatus, errorThrown) {
-				   alert('Something went wrong. ' + errorThrown);
-			      }
-			 });
-		    return false;
-	       });
-     });
+			var url = 'http://localhost/stats/Queries/?format=json'+args;
+			$.ajax({
+				type : 'POST',
+				url : 'http://localhost/stats/Queries/?format=json'+args,
+				dataType : 'json',
+				success : function(result) {
+					plotChart(result);
+				},
+				error : function(XMLHttpRequest, textStatus, errorThrown) {
+					alert('Something went wrong. ' + errorThrown);
+				}
+			});
+			return false;
+		});
+	});
+	/* plotChart with own Data !! */
 
-/* plotChart with own Data !! */     
+	function plotChart(dataArray) {
 
-function plotChart(dataArray) { 
-	 
-     /* dataset given, get the resulting array of the result object*/
-     var dataset = dataArray["result"];
-     
-     /* our dataArray contains data that needs to be kinda tweaked -> unix to javascripttime */
-     var dataToDisplay = [];
-     
-     for (var i in dataset) {	  
-	  dataToDisplay.push([i*1000,dataset[i]]);
-     }
+		/* dataset given, get the resulting array of the result object*/
+		var dataset = dataArray["result"];
 
-     if(dataToDisplay.length > 0){
-	  /* construct the x-axis array, again conversion from unix to javascripttime */
-	  var xArray = [];
+		/* our dataArray contains data that needs to be kinda tweaked -> unix to javascripttime */
+		var dataToDisplay = [];
 
-	  for(var i in dataset){
-	       xArray.push(i*1000);
-	  }
+		for (var i in dataset) {
+			dataToDisplay.push([i*1000,dataset[i]]);
+		}
 
-	  var data = [
-	       {
-		    //label: "Request logging",
-	       data: dataToDisplay
-	       }
-	       ];
-	  
-	  var options = {
-	  legend: {
-	       show: true,
-	       margin: 10,
-	       backgroundOpacity: 0.5
-	  },
-	  points: {
-	       show: true,
-	       radius: 3,
-	       clickable: true,
-	       hoverable: true
+		if(dataToDisplay.length > 0) {
+			/* construct the x-axis array, again conversion from unix to javascripttime */
+			var xArray = [];
 
-	  },
-	  bars: {
-	       show: true,
-	       hoverable: true
-	  },
-	  grid: {
-	       borderWidth:0,
-	       backgroundColor: "white",
-	       hoverable: true
-	  },
-	  xaxis: {
-	       ticks: xArray,
-	       mode: "time",
-	       timeformat: "%d/%m/%y"
-	  },
-	  yaxis: {
-	  }
-	  };
+			for(var i in dataset) {
+				xArray.push(i*1000);
+			}
 
-	  var plotarea = $("#placeholder");
-	  $.plot( plotarea , data, options );  
-	  function showTooltip(x, y, contents) {
-	       $('<div id="tooltip">' + contents + '</div>').css( {
-		    position: 'absolute',
-			      display: 'none',
-			      top: y + 8,
-			      left: x + 8,
-			      border: '1px solid #fdd',
-			      padding: '2px',
-			      'background-color': '#fee',
-			      opacity: 0.80
-			      }).appendTo("body").fadeIn(200);
-	  }
- 
-	  var previousPoint = null;
-	  $("#placeholder").bind("plothover", function (event, pos, item) {
-		    if (item) {
-			 if (previousPoint != item.dataIndex) {
-			      previousPoint = item.dataIndex;
-                    
-			      $("#tooltip").remove();
-			      var javascripttime = item.datapoint[0], yvalue = item.datapoint[1];			      
-			      var date        = new Date(javascripttime);
-			      var month       = date.getMonth()+1;
-			      var day         = date.getDate();
-			      var year        = date.getFullYear();
-			      var type        = $('#datasource').val();
-			      
-			      showTooltip(item.pageX, item.pageY,
-					  yvalue + " " + type + " on " + day + "/"+month+"/"+year);
-			 }
-		    }else{
-			 $("#tooltip").remove();
-			 previousPoint = null;
-		    }
-		    
-	       });
-     }else{
-	  $("#placeholder").text("No logging data available for the selected criteria.");
-     }
-};
+			var data = [{
+				//label: "Request logging",
+				data: dataToDisplay
+			}
+			];
 
-</script>
+			var options = {
+				legend: {
+					show: true,
+					margin: 10,
+					backgroundOpacity: 0.5
+				},
+				points: {
+					show: true,
+					radius: 3,
+					clickable: true,
+					hoverable: true
+
+				},
+				bars: {
+					show: true,
+					hoverable: true
+				},
+				grid: {
+					borderWidth:0,
+					backgroundColor: "white",
+					hoverable: true
+				},
+				xaxis: {
+					ticks: xArray,
+					mode: "time",
+					timeformat: "%d/%m/%y"
+				},
+				yaxis: {
+				}
+			};
+
+			var plotarea = $("#placeholder");
+			$.plot( plotarea , data, options );
+			function showTooltip(x, y, contents) {
+				$('<div id="tooltip">' + contents + '</div>').css({
+					position: 'absolute',
+					display: 'none',
+					top: y + 8,
+					left: x + 8,
+					border: '1px solid #fdd',
+					padding: '2px',
+					'background-color': '#fee',
+					opacity: 0.80
+				}).appendTo("body").fadeIn(200);
+			}
+
+			var previousPoint = null;
+			$("#placeholder").bind("plothover", function (event, pos, item) {
+				if (item) {
+					if (previousPoint != item.dataIndex) {
+						previousPoint = item.dataIndex;
+
+						$("#tooltip").remove();
+						var javascripttime = item.datapoint[0], yvalue = item.datapoint[1];
+						var date        = new Date(javascripttime);
+						var month       = date.getMonth()+1;
+						var day         = date.getDate();
+						var year        = date.getFullYear();
+						var type        = $('#datasource').val();
+
+						showTooltip(item.pageX, item.pageY,
+						yvalue + " " + type + " on " + day + "/"+month+"/"+year);
+					}
+				} else {
+					$("#tooltip").remove();
+					previousPoint = null;
+				}
+
+			});
+		} else {
+			$("#placeholder").text("No logging data available for the selected criteria.");
+		}
+	};</script>
 <?php
-include_once("templates/TheDataTank/footer.php");
-?>
-
+include_once ("templates/TheDataTank/footer.php");?>
 
