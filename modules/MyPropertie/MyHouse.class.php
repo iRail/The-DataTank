0989a9c4e2aa8a62abe49554e80281a555fa08bf
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-07-18 15:39:49 +0200

    Test module for kml printing and typing Locatable.

---
+++ modules/MyPropertie/MyHouse.class.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once("modules/AMethod.php");
+include_once("House.php");
+class MyHouse extends AMethod{
+
+	public function __construct(){
+		parent::__construct("MyHouse");
+	}
+
+	public static function getRequiredParameters(){
+		return array(); 
+	}
+
+	public static function getParameters(){
+		return array();
+	}
+
+	public function setParameter($name,$val){
+
+	}
+
+	public static function getDoc(){
+		return "This class returns a house object which implements the locatable interface".
+		     "This way we can test our kml printer";
+	}
+
+	public function call(){
+	     return new House();
+	}
+
+	public function allowedPrintMethods(){
+	     return array("kml");
+	}
+}
+?>
