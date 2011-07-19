16bfe434c2671377d63ae8e9afdabac6c111b8aa
abe <el.lauwer@gmail.com>
2011-07-19 19:52:46 +0200

    Feedback

---
+++ modules/Feedback/methods.php
@@ -0,0 +1,15 @@
+<?php
+/* Copyright (C) 2011 by iRail vzw/asbl
+ *
+ * Author: Werner Laurensse <el.lauwer aŧ gamil.com>
+ * License: AGPLv3
+ *
+ */
+
+class iRail{
+     public static $methods = array (
+          "Sessage"
+          );
+}
+
+?>
