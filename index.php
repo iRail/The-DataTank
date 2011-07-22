cbf5677af61cbe8d7c50116ba878af3b92544233
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-07-22 16:58:45 +0200

    Modifying new frontier receiver.php became index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+  /* Copyright (C) 2011 by iRail vzw/asbl
+   * Author: Werner Laurensse
+   * License: AGPLv3
+   */
+
+  /**
+   * This file contains the first frontier that dispatches requests to different method calls. This file will receive the call
+   * and return the result of that call.
+   * @package The-Datatank
+   * @copyright (C) 2011 by iRail vzw/asbl
+   * @license AGPLv3
+   * @author Werner Laurensse
+   */
+
+
 require_once('glue.php');
 require_once("printer/PrinterFactory.php");
 require_once("error/Exceptions.class.php");
@@ -13,86 +28,86 @@ date_default_timezone_set("UTC");
 
 
 $urls = array(
-    '/' => 'Index',
-    '/docs/' => 'Docs',
-    '/stats/' => 'Stats',
-    '/Feedback/Messages/(.*)/(.*)/' => 'FeedbackHandler',
-    '/(?P<module>.*?)/(?P<method>.*?)/.*' => 'ModuleHandler'
-);
+     '/' => 'Index',
+     '/docs/' => 'Docs',
+     '/stats/' => 'Stats',
+     '/Feedback/Messages/(.*)/(.*)/' => 'FeedbackHandler',
+     '/(?P<module>.*?)/(?P<method>.*?)/.*' => 'ModuleHandler'
+     );
 
 class Index {
-    function GET() {
-        require_once('contents.php');
-        include_once("templates/TheDataTank/header.php");
-        echo $index_content;
-        include_once("templates/TheDataTank/footer.php");
-    }
+     function GET() {
+	  require_once('contents.php');
+	  include_once("templates/TheDataTank/header.php");
+	  echo $index_content;
+	  include_once("templates/TheDataTank/footer.php");
+     }
 }
 
 class Docs {
-    function GET() {
-        require_once("docs/DocPrinter.php");
-    }
+     function GET() {
+	  require_once("docs/DocPrinter.php");
+     }
 }
 
 class Stats {
-    function GET() {
-        require_once("stats/index.php");
-    }
+     function GET() {
+	  require_once("stats/index.php");
+     }
 }
 
 class FeedbackHandler {
-    function GET() {
+     function GET() {
 
-    }
+     }
 }
 
 class ModuleHandler {
-    function GET($matches) {
-        //TODO add try and catch, throw error, logging.
-        try {
-            $result;
-            $module = $matches['module'];
-            $methodname = $matches['method'];
-            // Make sure that format is set and that the first letter is uppercase.
-            if (!isset($_GET['format'])) {
-                $_GET['format'] = 'Xml';
-            } else {
-                $_GET['format'] = ucfirst(strtolower($_GET['format']));
-            }
+     function GET($matches) {
+	  //TODO add try and catch, throw error, logging.
+	  try {
+	       $result;
+	       $module = $matches['module'];
+	       $methodname = $matches['method'];
+	       // Make sure that format is set and that the first letter is uppercase.
+	       if (!isset($_GET['format'])) {
+		    $_GET['format'] = 'Xml';
+	       } else {
+		    $_GET['format'] = ucfirst(strtolower($_GET['format']));
+	       }
 
-            if(file_exists("modules/$module/$methodname.class.php")) {
-                //get the new method
-                include_once ("modules/$module/$methodname.class.php");
-                $method = new $methodname();
+	       if(file_exists("modules/$module/$methodname.class.php")) {
+		    //get the new method
+		    include_once ("modules/$module/$methodname.class.php");
+		    $method = new $methodname();
 
-                // check if the given format is allowed by the method
-                // if not, throw an exception and return the allowed formats
-                // to the user.
-                if((!in_array(strtolower($_GET['format']),$method->getAllowedPrintMethods()))){
-                    throw new FormatNotAllowedTDTException($_GET['format'],$method::getAllowedPrintMethods());
-                }
+		    // check if the given format is allowed by the method
+		    // if not, throw an exception and return the allowed formats
+		    // to the user.
+		    if((!in_array(strtolower($_GET['format']),$method->getAllowedPrintMethods()))){
+			 throw new FormatNotAllowedTDTException($_GET['format'],$method::getAllowedPrintMethods());
+		    }
 
-                //execute the method when no error occured
-                $result = $method->call();
-            } else if (array_key_exists($module,ProxyModules::$modules)) {
-                //If we cannot find the modulename locally, we're going to search for it through proxy
-                $result = ProxyModules::call($module, $methodname, $_GET);		
-            } else {
-                echo 'test: ' . $module . $methodname;
-                throw new MethodOrModuleNotFoundTDTException($module . "/" .$methodname);
-            }
+		    //execute the method when no error occured
+		    $result = $method->call();
+	       } else if (array_key_exists($module,ProxyModules::$modules)) {
+		    //If we cannot find the modulename locally, we're going to search for it through proxy
+		    $result = ProxyModules::call($module, $methodname, $_GET);		
+	       } else {
+		    echo 'test: ' . $module . $methodname;
+		    throw new MethodOrModuleNotFoundTDTException($module . "/" .$methodname);
+	       }
 
-            $rootname = $methodname;
-            $rootname = strtolower($rootname);
-            $printer = PrinterFactory::getPrinter($rootname, $_GET['format'], $rootname, $result);
-            $printer->printAll();
-        } catch(Exception $e) {
-            ErrorHandler::logException($e);
-        }
+	       $rootname = $methodname;
+	       $rootname = strtolower($rootname);
+	       $printer = PrinterFactory::getPrinter($rootname, $_GET['format'], $rootname, $result);
+	       $printer->printAll();
+	  } catch(Exception $e) {
+	       ErrorHandler::logException($e);
+	  }
 
-        RequestLogger::logRequest();
-    }
+	  RequestLogger::logRequest();
+     }
 }
 
 glue::stick($urls);
