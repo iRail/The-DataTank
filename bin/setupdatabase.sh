f27d65663ccfd711e31dedd6b4372e35082d2823
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-08-03 14:27:21 +0200

    changed stats backend to a broader logging of seperate parts of the url

--- bin/setupdatabase.sh
+++ bin/setupdatabase.sh
@@ -12,36 +12,48 @@ NUMBER_OF_ARGS=2;
 if [ $# -eq $NUMBER_OF_ARGS ]
 then
 
-Q1=" use $2; CREATE TABLE IF NOT EXISTS errors (
+    Q1=" use $2; CREATE TABLE IF NOT EXISTS errors (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   time bigint(20) DEFAULT NULL,
   user_agent varchar(255) DEFAULT NULL,
   ip varchar(255) DEFAULT NULL,
   url_request varchar(255) DEFAULT NULL,
+  module varchar(64) DEFAULT NULL,
+  resource varchar(64) DEFAULT NULL,
+  format varchar(24) DEFAULT NULL,
+  subresources varchar(128) DEFAULT NULL,
+  reqparameters varchar(128) DEFAULT NULL,
+  allparameters varchar(164) DEFAULT NULL,
   error_message varchar(255) DEFAULT NULL,
   error_code varchar(255) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;"
 
-Q2="CREATE TABLE IF NOT EXISTS requests (
+    Q2="CREATE TABLE IF NOT EXISTS requests (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   time bigint(20) DEFAULT NULL,
   user_agent varchar(255) DEFAULT NULL,
   ip varchar(40) DEFAULT NULL,
-  url_request varchar(255) DEFAULT NULL,
+  url_request varchar(512) DEFAULT NULL,
+  module varchar(64) DEFAULT NULL,
+  resource varchar(64) DEFAULT NULL,
+  format varchar(24) DEFAULT NULL,
+  subresources varchar(128) DEFAULT NULL,
+  reqparameters varchar(128) DEFAULT NULL,
+  allparameters varchar(164) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;"
 
-Q3="CREATE TABLE IF NOT EXISTS feedback_messages (
+    Q3="CREATE TABLE IF NOT EXISTS feedback_messages (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   url_request varchar(255) DEFAULT NULL,
   msg text NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;"
 
-SQL="${Q1}${Q2}${Q3}"
+    SQL="${Q1}${Q2}${Q3}"
 
-mysql -u "$1" -p -e "$SQL"
+    mysql -u "$1" -p -e "$SQL"
 
 else
     echo -e "$NUMBER_OF_ARGS arguments should be passed along with the script. 
--- handlers/ModuleHandler.class.php
+++ handlers/ModuleHandler.class.php
@@ -17,8 +17,6 @@ class ModuleHandler {
 
     function GET($matches) {
 
-	RequestLogger::logRequest();
-
 	//always required: a module and a resource. This will always be given since the regex should be matched.
 	$module = $matches['module'];
 	$resourcename = $matches['resource'];
@@ -55,8 +53,11 @@ class ModuleHandler {
 	$RESTparameters = array();
 	if(isset($matches['RESTparameters'])){
 	    $RESTparameters = explode("/",$matches['RESTparameters']);
-	    array_pop($RESTparameters); // remove the last elemenet because that just contains the GET parameters
+	    array_pop($RESTparameters); // remove the last element because that just contains the GET parameters
 	}
+	
+	// for logging purposes
+	$requiredparams = array();
 	
 	foreach($resource->getRequiredParameters() as $parameter){
 	    //set the parameter of the method
@@ -64,6 +65,8 @@ class ModuleHandler {
 		throw new ParameterTDTException($parameter);
 	    }
 	    $resource->setParameter($parameter, $RESTparameters[0]);
+	    array_push($requiredparams,$RESTparameters[0]);
+	    
 	    //removes the first element and reindex the array
 	    array_shift($RESTparameters);
 	}
@@ -89,7 +92,9 @@ class ModuleHandler {
 
 	//Let's do the call!
 	$result = $resource->call();
-	
+	// for logging purposes
+	$subresources = array();
+
 	//Support RESTful URI lookups
 	foreach($RESTparameters as $resource){
 	    if(is_object($result) && isset($result->$resource)){
@@ -99,6 +104,7 @@ class ModuleHandler {
 	    }else{
 		break;//on error, just return what we have so far
 	    }
+	    array_push($subresources,$resource);
 	}
 	if(!is_object($result)){
 	    $o = new stdClass();
@@ -106,7 +112,10 @@ class ModuleHandler {
 	    $o->$RESTresource = $result;
 	    $result = $o;
 	}
- 
+	
+	// Log our succesful request
+	RequestLogger::logRequest($matches,$requiredparams,$subresources);
+
 	$printer = $this->printerfactory->getPrinter(strtolower($resourcename), $result);
 	$printer->printAll();
 	//this is it!
