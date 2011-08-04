1e84b0ed074099216e0b33dcee26454a9546f93f
abe <el.lauwer@gmail.com>
2011-08-04 19:02:43 +0200

    Added comments

--- test_rb.php
+++ test_rb.php
@@ -2,11 +2,11 @@
 include_once('Config.class.php');
 include_once('rb.php');
 
-// Config DB
+// This is how you configure it.
 R::setup('mysql:host=localhost;dbname=' . Config::$MySQL_DATABASE,
      Config::$MySQL_USER_NAME, Config::$MySQL_PASSWORD);
 
-// Create bean
+// This is how you create a bean.
 $message1 = R::dispense('feedback_messages');
 $message1->url_request = 'http://localhost/Test123';
 $message1->msg = 'test123';
@@ -20,15 +20,15 @@ $id2 = R::store($message2);
 echo 'Message 1 id: ' . $id1 . "\n";
 echo 'Message 2 id: ' . $id2 . "\n";
 
-// Get bean
+// This is how you get an existing bean.
 $message3 = R::load('feedback_messages', $id1);
 echo 'Message 1 & 3: ' . ($message1->getID() == $message3->getID()) . "\n";
 
-// Update bean
+// This is how you update a bean.
 $message2->url_request = 'http://localhost/Test678';
 $id2 = R::store($message2);
 
-// Find row
+// This is how you find a bean, no sql-injection possible.
 $message4 = R::findOne(
     'feedback_messages',
     'url_request = :url_request',
@@ -36,7 +36,7 @@ $message4 = R::findOne(
 );
 echo 'Message 2 & 4: ' . ($message2->getID() == $message4->getID()) . "\n";
 
-// Delete row
+// This is how you delete a bean.
 R::trash($message1);
 R::trash($message2);
 
