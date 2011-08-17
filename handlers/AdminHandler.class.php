8d65641f03d26042967fbd1af1fe6125088b6316
abe <el.lauwer@gmail.com>
2011-08-17 10:19:05 +0200

    Added hostname and subdir to path in AdminHandler to fix url bug.

--- handlers/AdminHandler.class.php
+++ handlers/AdminHandler.class.php
@@ -5,8 +5,10 @@ class AdminHandler {
         include_once ("templates/TheDataTank/header.php");
         echo '<h2>Admin</h2>';
         echo '<ul>';
-        echo '<li><a href="/admin/modules/">Modules</a></li>';
-        echo '<li><a href="/admin/resources/">Resources</a></li>';
+        echo '<li><a href="' . Config::$HOSTNAME . Config::$SUBDIR .
+            'admin/modules/">Modules</a></li>';
+        echo '<li><a href="' . Config::$HOSTNAME . Config::$SUBDIR .
+            'admin/resources/">Resources</a></li>';
         echo '</ul>';
         include_once ("templates/TheDataTank/footer.php");
     }
