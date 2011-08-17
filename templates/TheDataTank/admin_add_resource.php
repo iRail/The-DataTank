8e918b9cfabc76513094e64efcd29aa79fb70665
abe <el.lauwer@gmail.com>
2011-08-17 16:56:45 +0200

    Added csv resource tab.

--- templates/TheDataTank/admin_add_resource.php
+++ templates/TheDataTank/admin_add_resource.php
@@ -1,6 +1,4 @@
-<?php
 
-echo '
 <script>
 	$(function() {
 		$( "#tabs" ).tabs();
@@ -8,16 +6,36 @@ echo '
 </script>
 <h2>Add Resource</h2>
 <div id="tabs">
-	<ul>
+	<ul class>
 		<li><a href="#tabs-1">CSV</a></li>
-		<li><a href="#tabs-2">Escell</a></li>
 		<li><a href="#tabs-3">DB</a></li>
 	</ul>
-	<div id="tabs-1">csv</div>
-	<div id="tabs-2">excell</div>
+    <div id="tabs-1">
+        <form class="admin">
+            <div class="form_row">
+                <label class="admin">Name</label>
+                <input type="text" />
+            </div>
+            <div class="form_row">
+                <label class="admin">File</label>
+                <input type="file" />
+            </div>
+            <div class="form_row">
+                <label class="admin">Module</label>
+                <select name="module">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="fiat">Fiat</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </div>
+            <div class="form_row">
+                <label class="admin">Documentation</label>
+                <textarea cols="65" rows="10"></textarea>
+            </div>
+            <div><input type="submit" value="Save" /></div>
+            <input type="hidden" name="resource_type" value="csv" />
+        </form>
+    </div>
 	<div id="tabs-3">db</div>
 </div>
-
-';
-
-?>
--- templates/TheDataTank/header.php
+++ templates/TheDataTank/header.php
@@ -3,7 +3,7 @@
 	<meta charset="UTF-8">
 	<title>The DataTank</title>
 	<link rel="stylesheet" href="/<?php echo CONFIG::$SUBDIR ?>templates/TheDataTank/css/style.css"  media="screen"/>
-    <link type="text/css" href="/<?php echo CONFIG::$SUBDIR ?>templates/TheDataTank/css/ui-lightness/jquery-ui-1.8.15.custom.css" rel="Stylesheet" />	
+    <link type="text/css" href="/<?php echo CONFIG::$SUBDIR ?>templates/TheDataTank/css/custom-theme/jquery-ui-1.8.15.custom.css" rel="Stylesheet" />	
     <script type="text/javascript" src="/<?php echo CONFIG::$SUBDIR ?>templates/TheDataTank/js/jquery-1.6.2.js"></script>
     <script type="text/javascript" src="/<?php echo CONFIG::$SUBDIR ?>templates/TheDataTank/js/ui/jquery-ui-1.8.15.custom.js"></script>
 	<link rel="shortcut icon" href="/<?php echo CONFIG::$SUBDIR ?>favicon.ico" />
