9ff8de900fd54c42614305eef8adfe0c22072349
abe <el.lauwer@gmail.com>
2011-08-31 18:54:41 +0200

    Added admin

---
+++ tdt/static/admin.js
@@ -0,0 +1,30 @@
+var AdminAddModule = Backbone.Model.extend({
+
+});
+
+var AdminAddResource = Backbone.Model.extend({
+
+});
+
+$(document).ready(function() {
+    var upload = $('#admin-upload');
+    var file = $('#admin-file');
+    var uploadComputer = $('#admin-upload-computer');
+    var uploadLink = $('#admin-upload-link');
+    var removeUpload = $('#admin-remove-upload')
+
+    uploadComputer.click(function() {
+        upload.addClass('hidden');
+        file.removeClass('hidden');
+    });
+
+    uploadLink.click(function() {
+        upload.addClass('hidden');
+        file.removeClass('hidden');
+    });
+
+    removeUpload.click(function() {
+        file.addClass('hidden');
+        upload.removeClass('hidden');
+    });
+});
