afdfc37bf1bf4d75027dd9a311b9365fc6f8bc6c
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-09-14 18:43:14 +0200

    Forgot to include Packages resource.

---
+++ model/packages/TDTInfo/Packages.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This is a class which will return all the packages in The DataTank
+ * 
+ * @package The-Datatank/packages/TDTInfo
+ * @copyright (C) 2011 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jan Vansteenlandt <jan@iRail.be>
+ */
+
+class Packages extends AResource{
+
+    public static function getParameters(){
+	return array();
+    }
+
+    public static function getRequiredParameters(){
+	return array();
+    }
+
+    public function setParameter($key,$val){
+    }
+
+    public function call(){
+	$resmod = ResourcesModel::getInstance();
+	$o = $resmod->getAllPackages();
+	return $o;
+    }
+     
+    public static function getAllowedPrintMethods(){
+	return array("json","xml", "jsonp", "php", "html");
+    }
+
+    public static function getDoc(){
+	return "This resource contains the most important information";
+    }
+}
+
+?>
