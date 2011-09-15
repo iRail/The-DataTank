994bfa33c270489e0fc78f8624e8ce32cb140f39
Pieter Colpaert <pieter@iRail.be>
2011-09-15 15:12:30 +0200

    reformatted requestlogger

--- aspects/logging/RequestLogger.class.php
+++ aspects/logging/RequestLogger.class.php
@@ -1,20 +1,14 @@
 <?php
-
-  /**
-   * This file contains the RequestLogger.class.php
-   * @package The-Datatank/requests
-   * @copyright (C) 2011 by iRail vzw/asbl
-   * @license AGPLv3
-   * @author Jan Vansteenlandt <jan@iRail.be>
-   * @author Pieter Colpaert   <pieter@iRail.be>
-   */ 
+/**
+ * This file contains the RequestLogger.class.php
+ * @package The-Datatank/requests
+ * @copyright (C) 2011 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Jan Vansteenlandt <jan@iRail.be>
+ * @author Pieter Colpaert   <pieter@iRail.be>
+ */ 
  
-  /**
-   * This RequestLogger class logs every request to a certain method of a ceratin module.
-   * It will use a MySQL database and premade tables to store its data.
-   */
 class RequestLogger{
-
     /**
      * This function implements the logging part of the RequestLogger functionality.
      */
@@ -28,7 +22,6 @@ class RequestLogger{
         if(isset($_SERVER['HTTP_USER_AGENT'])){    
             $request->user_agent = $_SERVER['HTTP_USER_AGENT'];
         }
-        
         $request->ip = $_SERVER['REMOTE_ADDR'];
         $request->url_request = TDT::getPageUrl();
         $request->package = $matches["package"];
