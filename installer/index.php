1d246298d1cf7f25808b705c6931a9f96e40ba08
Jens Segers <segers.jens@gmail.com>
2011-09-15 11:20:14 +0200

    Detect if PHP is running

--- installer/index.php
+++ installer/index.php
@@ -1,4 +1,20 @@
+<?
+// check if PHP is running
+if ( false ) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Error: PHP is not running</title>
+</head>
+<body>
+	<h1>Error: PHP is not running</h1>
+	<p>The DataTank requires that your web server is running PHP. Your server does not have PHP installed, or PHP is turned off.</p>
+</body>
+</html>
 <?php
+}
 
 include_once("Installer.class.php");
 include_once("InstallController.class.php");
