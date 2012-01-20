137db970a7a75fa20385b6ff79364fa16d8d53a0
Pieter Colpaert <pieter@iRail.be>
2012-01-20 20:08:39 +0100

    Added spectql controller

---
+++ controllers/spectql/selectors/SPECTQLColumnName.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Implements a simple column name to select { ... , column }
+ *
+ * @package The-Datatank/controllers/spectql/selectors
+ * @copyright (C) 2011 by OKFN chapter Belgium vzw/asbl
+ * @license LGPL
+ * @author Pieter Colpaert
+ * @organisation Hogent
+ */
+class SPECTQLColumnName extends AArgument{
+
+    /**
+     * We will select the column from the resource and add it to the specified result. We will always take the distinct values after this operation; Let's not bother with that now.
+     */
+    public function execute(&$result,&$resourcearray){
+        $name = $this->name;
+        $i = 0;
+        foreach($resourcearray as $row){
+            if(isset($row[$name])){
+                $result[$i][$name] = $row[$name];
+            }
+            $i++;
+        }
+    }
+}
