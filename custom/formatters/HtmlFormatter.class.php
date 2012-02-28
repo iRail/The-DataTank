6f1df5333c4499c7f0b62b98600db4975c8f8a5f
Lieven Janssen <lieven.janssen@gmail.com>
2012-02-28 01:34:41 +0100

    Changed html formatter to table formatter and created dhtml and osm formatters

--- custom/formatters/HtmlFormatter.class.php
+++ custom/formatters/HtmlFormatter.class.php
@@ -12,7 +12,7 @@
  */
 
 /**
- * This class inherits from the abstract Formatter. It will generate a html-page with a print_r
+ * This class inherits from the abstract Formatter. It will generate a html table
  */
 class HtmlFormatter extends AFormatter {
 
@@ -23,31 +23,94 @@ class HtmlFormatter extends AFormatter {
     public function printHeader() {
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: text/html; charset=UTF-8");
+?>
+<head>
+    <link rel="stylesheet" type="text/css" href="http://cdn.sencha.io/ext-4.0.7-gpl/resources/css/ext-all.css" />
+    <style type="text/css">
+        #the-table { 
+            border:1px solid #bbb;
+            border-collapse:collapse; 
+        }
+        #the-table td,#the-table th { 
+            border:1px solid #ccc;
+            border-collapse:collapse;
+            padding:5px; 
+        }
+    </style>
+    <script type="text/javascript" src="http://cdn.sencha.io/ext-4.0.7-gpl/ext-all.js"></script>
+</head>	
+<?php		
     }
 
     public function printBody() {
-        //When the objectToPrint has a property Ontology, it is an RDF Model
-        //In this case, use the nice HTML formatting function
-        foreach ($this->objectToPrint as $class => $prop){
-            if (is_a($prop,"MemModel")){
-                $this->objectToPrint = $prop;
-                break;
-            }
-        }
-        
-        if (is_a($this->objectToPrint,"MemModel")) {
-            echo $this->objectToPrint->writeAsHTMLTable();
-        } else {
-            echo "<pre>";
-            print_r($this->objectToPrint);
-            echo "</pre>";
-        }
-    }
+?>
+	<!--	
+		<title></title>
+	-->
+
+<!--	
+    <script type="text/javascript" src="transform-dom.js"></script>
+-->	
+<body>
+Show as: 
+<a href="<?php echo $this->getUrl('dhtml') ?>">dhtml</a>
+<a href="<?php echo $this->getUrl('osm') ?>">osm</a>
+<a href="<?php echo $this->getUrl('json') ?>">json</a>
+<a href="<?php echo $this->getUrl('xml') ?>">xml</a>
+<a href="<?php echo $this->getUrl('kml') ?>">kml</a>
+<br/><br/>
+<table cellspacing="0" id="the-table" width="100%">
+<?php		
+		$result = "";
+		$array = get_object_vars($this->objectToPrint);
+		$thead = '<thead><tr style="background:#eeeeee;">';
+		$tbody = '<tbody>';
+		foreach($array as $key1 => $val1) {
+			if(is_object($val1)){
+				$array = get_object_vars($val1);
+			}
+			else if(is_array($val1)) {
+				$array = $val1;
+			}
+			$firstrow = true;
+			foreach($array as $key2 => $val2){
+				if(is_object($val2)){
+					$array = get_object_vars($val2);
+				}
+				else if(is_array($val2)) {
+					$array = $val2;
+				}
+				$tbody .= '<tr>';
+				foreach($array as $key3 => $val3){
+					if ($firstrow) {
+						$thead .= "<th>" . $key3 . "</th>";	
+					}
+					$tbody .= "<td>" . $val3 . "</td>";
+					//$result .= $key3 . " : " . $val3 . "</br>";				
+				}
+				$tbody .= '</tr>';
+				$firstrow = false;
+			}
+		}	
+		$thead .= '</tr></thead>';
+		$tbody .= '</tbody>';
+		echo $thead . $tbody;
+		//echo "hier";
+?>
+</table>
+</body>
+</html>
+<?php
+	}
     
     public static function getDocumentation(){
         return "The Html formatter is a formatter for developing purpose. It prints everything in the internal object.";
     }
-    
+
+	private function getUrl($type) {
+		$ext = explode(".", $_SERVER['REQUEST_URI']);
+		return "http://" . $_SERVER['HTTP_HOST'] . str_replace('.' . $ext[1],'.' . $type,$_SERVER['REQUEST_URI']);	
+	}
 }
 
 ;
