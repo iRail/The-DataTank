2a57a4fb8de37295500edf57259a9fb09142e8de
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2012-04-06 11:43:41 +0200

    Implemented issue #79.

--- custom/formatters/HtmlFormatter.class.php
+++ custom/formatters/HtmlFormatter.class.php
@@ -30,19 +30,6 @@ class HtmlFormatter extends AFormatter {
         header("Content-Type: text/html; charset=UTF-8");
 ?>
 <head>
-    <link rel="stylesheet" type="text/css" href="http://cdn.sencha.io/ext-4.0.7-gpl/resources/css/ext-all.css" />
-    <style type="text/css">
-        #the-table { 
-            border:1px solid #bbb;
-            border-collapse:collapse; 
-        }
-        #the-table td,#the-table th { 
-            border:1px solid #ccc;
-            border-collapse:collapse;
-            padding:5px; 
-        }
-    </style>
-    <script type="text/javascript" src="http://cdn.sencha.io/ext-4.0.7-gpl/ext-all.js"></script>
 </head>	
 <?php		
     }
@@ -54,67 +41,57 @@ class HtmlFormatter extends AFormatter {
 	-->
 
 <!--	
-    <script type="text/javascript" src="transform-dom.js"></script>
+            
 -->	
 <body>
-<table cellspacing="0" id="the-table" width="100%">
-<?php		
-		$result = "";		
-		$array = get_object_vars($this->objectToPrint);
-		$this->thead = '<thead><tr style="background:#eeeeee;">';
-		$this->tbody = '<tbody>';
-		$this->getPrintData($array);		
-		$this->thead .= '</tr></thead>';
-		$this->tbody .= '</tbody>';
-		echo $this->thead . $this->tbody;
-		
+
+<?php
+            echo $this->displayTree($this->objectToPrint);
 ?>
-</table>
+
 </body>
 </html>
 <?php
-	}
+      }
     
-    public static function getDocumentation(){
-        return "The Html formatter is a formatter for developing purpose. It prints everything in the internal object.";
-    }
+public static function getDocumentation(){
+    return "The Html formatter is a formatter for developing purpose. It prints everything in the internal object.";
+}
 
-	private function getUrl($type) {
-		$ext = explode(".", $_SERVER['REQUEST_URI']);
-		return "http://" . $_SERVER['HTTP_HOST'] . str_replace('.' . $ext[1],'.' . $type,$_SERVER['REQUEST_URI']);	
-	}
+private function getUrl($type) {
+    $ext = explode(".", $_SERVER['REQUEST_URI']);
+    return "http://" . $_SERVER['HTTP_HOST'] . str_replace('.' . $ext[1],'.' . $type,$_SERVER['REQUEST_URI']);	
+}
 
-	private function getPrintData($array) {
-		$firstrow = true;
-		foreach($array as $key => $val){
-			if(is_object($val)){
-				$array = get_object_vars($val);
-				$this->getPrintData($array);
-				if ($this->rowcontents != "") {
-					if ($firstrow) {
-						$this->thead .= $this->headcontents;
-					}
-					$this->tbody .= '<tr>' . $this->rowcontents . '</tr>';
-				}
-				$this->rowcontents = "";
-				$this->headcontents = "";
-			} else if(is_array($val)) {
-				$array = $val;
-				$this->getPrintData($array);
-				if ($this->rowcontents != "") {
-					if ($firstrow) {
-						$this->thead .= $this->headcontents;
-					}
-					$this->tbody .= '<tr>' . $this->rowcontents . '</tr>';
-				}
-				$this->rowcontents = "";
-				$this->headcontents = "";
-			} else {
-				$this->headcontents .= "<th>" . $key . "</th>";	
-				$this->rowcontents .= "<td>" . $val . "</td>";
-			}
-			$firstrow = false;			
-		}
-	}	
+private function displayTree($var) {
+     $newline = "\n";
+     $output ="";
+     foreach($var as $key => $value) {
+         if (is_array($value) || is_object($value)) {
+             $value = $newline . "<ul>" . $this->displayTree($value) . "</ul>";
+         }
+
+         if (is_array($var)) {
+             if (!stripos($value, "<li")) {
+                $output .= "<li>" . $value . "</li>" . $newline;
+             }
+             else {
+                $output .= $value . $newline;
+             }
+         
+         }
+         else { // is_object
+            if (!stripos($value, "<li")) {
+               $value = "<ul><li>" . $value . "</li></ul>" . $newline;
+            } 
+            
+            $output .= "<li>" . $key . $value . "</li>" . $newline;
+         }
+         
+     }
+     return $output;
+}
+
+
 }
 ?>
