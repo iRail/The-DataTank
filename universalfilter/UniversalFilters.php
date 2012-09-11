5f3144989208317a6ef36ccfc3bf7c882abf1da5
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2012-09-11 15:46:35 +0200

    A commit just in case, limit executer has been written, now i have to adjust the tree cloner, debugger and the lime file.

--- universalfilter/UniversalFilters.php
+++ universalfilter/UniversalFilters.php
@@ -229,17 +229,37 @@ class DistinctFilter extends NormalFilterNode{
 }
 
 /**
- * Represents a distinct filter => keeps only the rows that are distinct
+ * Represents a limit filter => keeps a certain amount of rows from a certain offset
+ * Note that rows start counting from 0 thus limit(0,10) will return the first 10 rows.
  *
  * type: Table -> Table
  *
- * aka "DISTINCT"
+ * aka "LIMIT"
  */
 class LimitFilter extends NormalFilterNode{
-    public function __construct(UniversalFilterNode $source=null) {
+    public function __construct(UniversalFilterNode $source=null, $offset,$limit) {
         parent::__construct("FILTERLIMIT");
         if($source!=null) $this->setSource($source);
+        $this->offset = $offset;
+        $this->limit = $limit;
     }
+
+    public function setOffset($offset){
+        $this->offset = $offset;
+    }
+    
+    public function getOffset(){
+        return $this->offset;
+    }
+    
+    public function setLimit($limit){
+        $this->limit = $limit;
+    }
+    
+    public function getLimit(){
+        return $this->limit;
+    }
+
 }
 
 
